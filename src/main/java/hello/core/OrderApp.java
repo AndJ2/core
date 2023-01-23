c2b4c2d2365b87dcf2ae05ef02a08ae1e7557372
andJ <zia7894@naver.com>
2023-01-23 21:27:46 +0900

    스프링 컨테이너와 스프링 빈 - 다양한 설정 형식 지원_자바 코드, XML

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -1,7 +1,7 @@
 package hello.core;
 
-import hello.core.Order.Order;
-import hello.core.Order.OrderService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
--- src/test/java/hello/core/member/Order/OrderServiceTest.java
+++ src/test/java/hello/core/member/Order/OrderServiceTest.java
@@ -1,13 +1,11 @@
 package hello.core.member.Order;
 
 import hello.core.AppConfig;
-import hello.core.Order.Order;
-import hello.core.Order.OrderService;
-import hello.core.Order.OrderServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
