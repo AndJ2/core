edb629727ace9d0c078dcd4594ad11044924543e
andJ <zia7894@naver.com>
2023-01-26 12:36:10 +0900

    정리

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -2,6 +2,7 @@ package hello.core.xml;
 
 import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -10,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class XmlAppContext {
     @Test
+    @DisplayName("xml 이용")
     void xmlAppContext() {
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
         MemberService memberService = ac.getBean("memberService", MemberService.class);
